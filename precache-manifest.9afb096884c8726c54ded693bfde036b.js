30fe432346068dc2b18032660d341b20cc554439
ahmad-kajjan <ahmadkajjan73@gmail.com>
2020-03-23 18:10:30 +0300

    Updates

---
+++ precache-manifest.9afb096884c8726c54ded693bfde036b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb91006d7dbba890d63b3ba7f1dfce1e",
+    "url": "/arabiadex/index.html"
+  },
+  {
+    "revision": "0f83e80819fac9b5ff20",
+    "url": "/arabiadex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35697bc4be19fbad5b1e",
+    "url": "/arabiadex/static/js/2.26e5311d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/arabiadex/static/js/2.26e5311d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f83e80819fac9b5ff20",
+    "url": "/arabiadex/static/js/main.c6ddd77a.chunk.js"
+  },
+  {
+    "revision": "8b9431416b16a0b37a1f",
+    "url": "/arabiadex/static/js/runtime-main.cc1227cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/arabiadex/static/media/logo.5d5d9eef.svg"
+  }
+]);
