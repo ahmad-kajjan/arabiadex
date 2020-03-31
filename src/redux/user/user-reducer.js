e46e3947cadc4a5a15c12274acf481858aacd838
ahmad-kajjan <ahmadkajjan73@gmail.com>
2020-03-31 20:44:23 +0300

    change user-reduser to handel three users

--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -1,20 +1,62 @@
 const INITAL_STATE = {
-    currentUser:null,
-    currencyBalance:null
+    firstUser:null,
+    firstUserPrivateKey:null,
+    firstUserBalance:null,
+    secondUser:null,
+    secondUserPrivateKey:null,
+    secondUserBalance:null,
+    contractUser:null,
+    contractUserPrivateKey:null,
+    contractUserBalance:null
 }
 const userReducer = (state=INITAL_STATE,action) =>{
     switch (action.type) {
-            case 'SET_CURRENCY_BALANCE':
+            case 'SET_FIRST_USER_BALANCE':
                 return {
                     ...state,
-                    currencyBalance:action.payload
+                    firstUserBalance:action.payload
                 }
-            case 'SET_CURRENT_USER':
+            case 'SET_FIRST_USER':
                 return {
                     ...state,
-                    currentUser:action.payload
+                    firstUser:action.payload
                 }
-    
+            case 'SET_FIRST_USER_PRIVATE_KEY':
+                return {
+                    ...state,
+                    firstUserPrivateKey:action.payload
+                }
+            case 'SET_SECOND_USER':
+                return {
+                    ...state,
+                    secondUser:action.payload
+                }
+            case 'SET_SECOND_USER_PRIVATE_KEY':
+                return {
+                    ...state,
+                    secondUserPrivateKey:action.payload
+                }
+            case 'SET_SECOND_USER_BALANCE':
+                return {
+                    ...state,
+                    secondUserBalance:action.payload
+                }
+            case 'SET_CONTRACT_USER':
+                return {
+                     ...state,
+                    contractUser:action.payload
+                 }
+            case 'SET_CONTRACT_USER_PRIVATE_KEY':
+                return {
+                    ...state,
+                    contractUserPrivateKey:action.payload
+                }
+            case 'SET_SECOND_USER_BALANCE':
+                return {
+                    ...state,
+                    contractUserBalance:action.payload
+               }                           
+        
         default:
             return state;
     }
